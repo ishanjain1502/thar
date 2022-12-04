4edbdf86795d41cc64d391af3f4ee74fffd41cd4
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-12-04 14:30:17 +0530

    minor fixes

--- components/NavBar.js
+++ components/NavBar.js
@@ -21,7 +21,7 @@ export const NavBar = () => {
             <img
               src="/assets/images/main/navBanner.png"
               alt="Thar 2023 Navigation Logo"
-              className="pointer-events-none"
+              className="pointer-events-none w-auto sm:w-10/12 lg:w-auto"
               draggable="false"
             />
           </Link>
@@ -37,7 +37,7 @@ export const NavBar = () => {
               className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline"
               onClick={() => setIsMenuOpen(true)}
             >
-              <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+              <svg className="w-5 text-white" viewBox="0 0 24 24">
                 <path
                   fill="currentColor"
                   d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
@@ -56,7 +56,7 @@ export const NavBar = () => {
         </div>
         {isMenuOpen && (
           <div className="absolute top-0 left-0 w-full">
-            <div className="p-5 bg-black rounded shadow-sm">
+            <div className="p-5 bg-black rounded shadow-sm min-h-screen">
               <div className="flex items-center justify-between mb-4">
                 <div>
                   <a
@@ -77,10 +77,10 @@ export const NavBar = () => {
                   <button
                     aria-label="Close Menu"
                     title="Close Menu"
-                    className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                    className="p-2 -mt-2 -mr-2 transition duration-200 rounded"
                     onClick={() => setIsMenuOpen(false)}
                   >
-                    <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+                    <svg className="w-5 text-white" viewBox="0 0 24 24">
                       <path
                         fill="currentColor"
                         d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
@@ -121,13 +121,13 @@ const NavItemPrimary = ({ href, text }) => {
 const NavItemSecondary = ({ href, text }) => {
   return (
     <li>
-      <a
+      <Link
         href={href}
         className="inline-flex items-center justify-center w-full font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
         title={text}
       >
         {text}
-      </a>
+      </Link>
     </li>
   );
 };
