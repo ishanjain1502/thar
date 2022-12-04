50aa9bd572831b606a8b564d02de07e12738fa9f
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-12-05 01:08:17 +0530

    added become ca button

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Link from "next/link";
 import About from "../components/home/About";
 import Hero from "../components/home/Hero";
 import { NavBar } from "../components/NavBar";
@@ -15,6 +16,43 @@ export default function Home() {
         <NavBar />
         <Hero />
         <About />
+        <div className="flex justify-center items-center my-20 relative h-auto">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="hidden sm:block w-14 sm:w-16 md:w-20 lg:w-28 xl:w-32 aspect-square stroke-yellow-300 animate-pulse"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5"
+            />
+          </svg>
+
+          <Link
+            href="/ca"
+            className="font-spaceboards text-xl md:text-2xl xl:text-3xl px-10 sm:px-12 lg:px-14 xl:px-16 py-6 sm:py-6 xl:py-8 border-4 border-yellow-300 bg-black/20 backdrop-blur-xl hover:text-yellow-300 transition-all ease-in-out"
+          >
+            <span className="hidden sm:inline-block">Become</span>&nbsp;Campus Ambassador
+          </Link>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="hidden sm:block w-14 sm:w-16 md:w-20 lg:w-28 xl:w-32 aspect-square stroke-yellow-300 animate-pulse"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5"
+            />
+          </svg>
+        </div>
       </main>
       <div className="h-[100vh] overflow-hidden fixed left-0 top-0 z-[1]">
         <video
