3c6d6066146a9eb7b57d060ac9a5ae1d836f398f
AniruddhaSoni <anirudh.soni007@gmail.com>
2022-12-05 16:01:11 +0530

    added highlight seciton

--- components/home/Highlight.jsx
+++ components/home/Highlight.jsx
@@ -1,30 +1,62 @@
+import Image from "next/image";
 import React from "react";
 
 export default function Highlights() {
   return (
-    <div id="theme" className="flex justify-center mb-10">
+    <div id="highlights" className="flex justify-center mb-10">
       {/* Flex Break Point - lg */}
       <div className="w-11/12 sm:w-10/12 md:w-8/12 xl:w-8/12 border-4 border-yellow-300 flex flex-col-reverse lg:flex-row">
-        <article
-          lang="en"
-          className="w-full lg:w-9/12 p-8 text-xl md: bg-black/20 backdrop-blur-2xl hyphens"
-        >
-          Granted that you are indefinitely trivial and your kind have only
-          existed for a minuscule time, you are a salient part of something very
-          immense and alluring. Every single element on earth was created at the
-          heart of a star. Big bang manifests our physical composition out of
-          this cosmos. Our bodies are quite literally made of remnants of stars
-          and explosions in the galaxy. There&apos;s Stardust gushing through
-          your veins and galaxies swirling through your soul. So the next time
-          you look up at the stars, remember that the beauty of the night sky is
-          within you. Importing this indelible galaxy on land, we introduce Thar
-          &apos;23, welcoming you to encounter the bizzare gush of utterly
-          prepossessing and striking events.
-        </article>
         <div className=" bg-yellow-300/20 backdrop-blur-xl w-full lg:w-3/12 flex h-full justify-center items-center lg:border-r-4 border-b-4 lg:border-b-0 border-yellow-300">
-          <span className="lg:rotate-90 h-max text-4xl font-spaceboards my-4 pt-3 text-center">
-            The Galaxy Indelible
+          <span className="lg:-rotate-90 h-max text-4xl font-spaceboards my-4 pt-3 text-center">
+            Highlights
           </span>
+        </div>
+        <div
+          lang="en"
+          className="w-full lg:w-9/12 p-8 text-xl md: bg-black/20 backdrop-blur-2xl hyphens flex gap-5 overflow-x-scroll no-scrollbar"
+        >
+          <Image
+            src="/assets/images/highlight/1.JPG"
+            alt=""
+            height={300}
+            width={300}
+          />
+          <Image
+            src="/assets/images/highlight/2.JPG"
+            alt=""
+            height={300}
+            width={300}
+          />
+          <Image
+            src="/assets/images/highlight/3.JPG"
+            alt=""
+            height={300}
+            width={300}
+          />
+          <Image
+            src="/assets/images/highlight/4.JPG"
+            alt=""
+            height={300}
+            width={300}
+          />
+          <Image
+            src="/assets/images/highlight/5.JPG"
+            alt=""
+            height={300}
+            width={300}
+          />
+          <Image
+            src="/assets/images/highlight/6.JPG"
+            alt=""
+            height={300}
+            width={300}
+          />
+          <Image
+            src="/assets/images/highlight/7.JPG"
+            alt=""
+            height={300}
+            width={300}
+          />
         </div>
       </div>
     </div>
