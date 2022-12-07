d1e6278a9e4db45241e69e2349aa2ec5bf327466
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-12-08 00:19:48 +0530

    added sponsors section

---
+++ pages/sponsors/index.js
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import Image from "next/image";
+import React from "react";
+import { NavBar } from "../../components/NavBar";
+
+export default function index() {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="flex flex-col items-center">
+        <NavBar />
+        <p className="pt-32 lg:pt-48 pb-6 text-center font-spaceboards text-yellow-300 text-5xl md:text-7xl">
+          SPONSORS
+          <br />
+          <span className="text-xl text-white">Our Previous Sponsors</span>
+        </p>
+        <div className="flex justify-center max-w-screen flex-wrap gap-8 w-full pb-20">
+          <div className="w-11/12 md:w-8/12 aspect-[1080/531] relative">
+            <Image src="/assets/images/sponsors/prev.png" fill />
+          </div>
+          <div className="w-full aspect-[1080/531] relative">
+            <Image src="/assets/images/sponsors/categories.png" fill />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
