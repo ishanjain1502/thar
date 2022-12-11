bfdeb5889d08185aef4390ce5e4035603e788b3e
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-12-11 10:20:31 +0530

    About text fix

--- components/ca/Form.js
+++ components/ca/Form.js
@@ -2,8 +2,9 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="" id="form">
-      Form here
+    <div id="aboutca" className="flex justify-center my-12 md:my-14 lg:my-16">
+      {/* Flex Break Point - lg */}
+      <div className="w-11/12 sm:w-10/12 md:w-8/12 xl:w-8/12 border-4 border-yellow-300 flex flex-col lg:flex-row"></div>
     </div>
   );
 }
