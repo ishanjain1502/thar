9981ec009a05069d9f0793c88f06670ba006d1af
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-12-11 07:55:23 +0530

    Previous layout Cleanup

--- pages/ca.js
+++ pages/ca.js
@@ -1,8 +1,5 @@
 import Head from "next/head";
 import React from "react";
-import About from "../components/ca/About";
-import Form from "../components/ca/Form";
-import Hero from "../components/ca/Hero";
 import Footer from "../components/globals/Footer";
 import { NavBar } from "../components/globals/NavBar";
 
@@ -13,10 +10,8 @@ export default function ca() {
         <title>Campus Ambassador - Thar 2023</title>
       </Head>
       <NavBar />
-      <Hero />
-      <Form />
-      <About />
-      <Footer/>
+
+      <Footer />
     </>
   );
 }
