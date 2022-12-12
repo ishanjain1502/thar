71e13dbeac10bbd96ecb017abf5fd863a61e3dcd
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-12-12 23:33:13 +0530

    become CA button fix

--- components/home/BecomeCA.js
+++ components/home/BecomeCA.js
@@ -10,7 +10,7 @@ export default function BecomeCA() {
         viewBox="0 0 24 24"
         strokeWidth={1.5}
         stroke="currentColor"
-        className="hidden sm:block w-14 sm:w-16 md:w-20 lg:w-28 xl:w-32 aspect-square stroke-yellow-300 animate-pulse"
+        className="block w-14 sm:w-16 md:w-20 lg:w-28 xl:w-32 aspect-square stroke-yellow-300 animate-pulse"
       >
         <path
           strokeLinecap="round"
@@ -21,10 +21,8 @@ export default function BecomeCA() {
 
       <Link
         href="/ca"
-        className="font-spaceboards text-xl md:text-2xl xl:text-3xl px-10 sm:px-12 lg:px-14 xl:px-16 py-6 sm:py-6 xl:py-8 border-4 border-yellow-300 bg-black/20 backdrop-blur-xl hover:text-yellow-300 transition-all ease-in-out"
-      >
-        <span className="hidden sm:inline-block">Become</span>&nbsp;Campus
-        Ambassador
+        className="font-spaceboards max-w-[91.666667%] text-xl md:text-2xl xl:text-3xl px-10 sm:px-12 lg:px-14 xl:px-16 py-6 sm:py-6 xl:py-8 border-4 border-yellow-300 bg-black/20 backdrop-blur-xl hover:text-yellow-300 transition-all ease-in-out text-center"
+      > BECOME CAMPUS AMBASSADOR
       </Link>
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -32,7 +30,7 @@ export default function BecomeCA() {
         viewBox="0 0 24 24"
         strokeWidth={1.5}
         stroke="currentColor"
-        className="hidden sm:block w-14 sm:w-16 md:w-20 lg:w-28 xl:w-32 aspect-square stroke-yellow-300 animate-pulse"
+        className="block w-14 sm:w-16 md:w-20 lg:w-28 xl:w-32 aspect-square stroke-yellow-300 animate-pulse"
       >
         <path
           strokeLinecap="round"
