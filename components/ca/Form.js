cf8b2d79e391751e7d7cabf9c9e7a29a73090c43
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2022-12-13 00:07:53 +0530

    button fix

--- components/ca/Form.js
+++ components/ca/Form.js
@@ -289,7 +289,7 @@ export default function About() {
                   <div className="flex justify-end">
                     <button
                       type="submit"
-                      className=" bg-yellow-300 text-black  focus:outline-none font-medium text-sm px-5 py-2.5 text-center mr-2 inline-flex items-center font-spaceboards"
+                      className="transition-all mt-2 bg-yellow-300 text-black  focus:outline-none font-medium text-sm px-5 py-2.5 text-center mr-2 inline-flex items-center font-spaceboards active:scale-95 outline-none"
                     >
                       Submit Details
                     </button>
