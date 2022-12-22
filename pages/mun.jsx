4f44dc2aa794d504fd712b79bd05978f048fbaf3
AniruddhaSoni <anirudh.soni007@gmail.com>
2022-12-22 23:02:50 +0530

    fixed warnings and added MUN page

---
+++ pages/mun.jsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import React from "react";
+
+export default function MUN() {
+  return (
+    <>
+      <Head>
+        <title>MUN | Model United Nation</title>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+    </>
+  );
+}
