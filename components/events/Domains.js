4b7f7721ff0484285c1f0dca5e0dee89efcbffab
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2022-12-28 20:44:59 +0530

    data in action

---
+++ components/events/Domains.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import React from "react";
+import { domains_data } from "../../data/domains";
+
+export default function Domains({ id }) {
+  return (
+    <div
+      key={id}
+      className="flex flex-col sm:flex-row w-full bg-zinc-900 backdrop-blur-lg max-w-sm sm:max-w-full md:max-w-3xl lg:max-w-4xl rounded-md overflow-hidden ring-2 ring-yellow-300"
+    >
+      <div className="ring-1 aspect-square w-full sm:w-6/12"></div>
+      <div className="ring-1 w-6/12 p-4 justify-between py-10 flex flex-col">
+        <span>
+          <p className="text-4xl font-spaceboards text-yellow-300 uppercase">
+            {id.replaceAll('_',' ')}
+          </p>
+          <article className="text-lg mt-2 text-zinc-300">
+            {domains_data[id]}
+          </article>
+        </span>
+        <Link href={`/events/${id}`} passHref legacyBehavior>
+          <a className="px-6 py-2 ring mt-4 rounded-md font-bold bg-yellow-300 hover:bg-yellow-300/90 hover:text-black active:scale-95 transition-all ease-in-out text-yellow-900 ring-yellow-200/30 uppercase">
+            View Events
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+}
--- components/events/EventWrapper.js
+++ components/events/EventWrapper.js
@@ -1,33 +1,13 @@
-import Link from "next/link";
-import React, { useEffect } from "react";
-
-import Event from "./Event";
+import React from "react";
+import { domains_data } from "../../data/domains";
+import Domains from "./Domains";
 
 export default function EventWrapper() {
   return (
-    <div className="relative mx-auto py-8 md:py-16 px-4 w-full max-w-7xl flex flex-col items-center">
-      <div className="flex flex-col sm:flex-row w-full bg-zinc-900 backdrop-blur-lg max-w-sm sm:max-w-full md:max-w-3xl lg:max-w-4xl rounded-md overflow-hidden ring-2 ring-yellow-300">
-        <div className="ring-1 aspect-square w-full sm:w-6/12"></div>
-        <div className="ring-1 w-6/12 p-4 justify-center flex flex-col">
-          <p className="text-4xl font-spaceboards text-yellow-300 uppercase">
-            Aeromodeling
-          </p>
-          <article className="text-lg mt-2 text-zinc-300">
-            To soar the sky has been humankind's ultimate wish since the
-            beginning of our time. Bringing this spirit to our festival, we
-            present a plethora of events where you can test your aerodynamics
-            and innovation skills. Contribute to the practical understanding of
-            science by immersing yourself in this occasion. Replicate the flight
-            of your dreams with the finest of people having the finest of
-            skills.
-          </article>
-          <Link href="as" passHref legacyBehavior>
-            <a className="px-6 py-2 ring mt-4 rounded-md font-bold bg-yellow-300 hover:bg-yellow-300/90 hover:text-black active:scale-95 transition-all ease-in-out text-yellow-900 ring-yellow-200/30 uppercase">
-              View Events
-            </a>
-          </Link>
-        </div>
-      </div>
+    <div className="relative mx-auto py-8 md:py-16 px-4 w-full max-w-7xl flex flex-col items-center gap-10">
+      {Object.keys(domains_data).map((domain) => (
+        <Domains id={domain} />
+      ))}
     </div>
   );
 }
