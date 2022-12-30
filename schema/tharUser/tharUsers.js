dff8e257010146d672105710a9c78701796fa442
rahul meena <rahulbadgoti000@gmail.com>
2022-12-31 02:00:49 +0530

    userSchema added

--- schema/tharUser/tharUsers.js
+++ schema/tharUser/tharUsers.js
@@ -1,6 +1,9 @@
-// name, email, phone, college, rollNo, degree, address, pincode
+// name, email, phone, college, rollNo, degree, address, pincode, refferedBy(CA-ID),Events(all registered), team-mates name(optional)
 import mongoose from "mongoose";
-const userSchema = new mongoose.Schema({
+
+
+
+const tharUserSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -57,27 +60,26 @@ const userSchema = new mongoose.Schema({
         minLength: 6,
         maxLength: 6,
     },
-    referralCode: {
+    userTharID: {
         type: String,
         required: true,
         minLength: 9,
-        maxLength: 9,
+        maxLength: 20,
         unique: true
     },
-    referralCount: {
-        type: Number,
-        default: 0,
-        min: 0,
-        max: 1000
+    referedBy: {
+        type: mongoose.Schema.ObjectId,
+        ref: "user" ,
+        required: true,        
     },
-    referedTharUser: {
-        type: mongoose.Schema.ObjectId,        
-        ref: 'tharUser' ,
-        required: true,
+    referedCode: {
+        type: String,
+        minLength: 9,
+        maxLength: 9,     
     }
 }, {
     timestamps: true,
 })
 
-const user = mongoose.models.User || mongoose.model('User', userSchema);
-export default user;
+const tharUser = mongoose.models.tharUser || mongoose.model('tharUser', tharUserSchema);
+export default tharUser;
