d11845c75285401980816b2e7ce6bd50b27ca321
Ishan Jain <ishanjain1502@gmail.com>
2023-01-14 14:48:09 +0530

    validations --gokart

--- validators/v1/events/gokart.js
+++ validators/v1/events/gokart.js
@@ -2,27 +2,29 @@ import Joi from "joi";
 
 const validationSchema = Joi.object({
     teamName : Joi.string().trim().min(3).max(20).required(),
-    captainName : Joi.string().trim().min(3).max(30).required(),
+    captainName : Joi.string().trim().min(3).max(30).required(), 
     captainPhoneNo : Joi.string().trim().min(10).max(13).required(),
-    captainEmail : Joi.string().trim().min(16).max(50).required().pattern(new RegExp('^[A-Za-z0-9](\.?[A-Za-z0-9]){5,}@gmail\.com$')).message('Enter a valid Gmail address'),
+    captainEmail : Joi.string().trim().min(16).max(50).required()
+    .pattern(new RegExp('^[A-Za-z0-9](\.?[A-Za-z0-9]){5,}@gmail\.com$'))
+    .message('Enter a valid Gmail address'),
     driverName : Joi.string().trim().min(3).max(30).required(),
     driverPhoneNo : Joi.string().trim().min(10).max(13).required(),
-    driverEmail : Joi.string().email().trim().min(16).max(50).required(),
+    driverEmail : Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).trim().min(16).max(50).required(),
     coDriverName : Joi.string().trim().min(3).max(30).required(),
     coDriverPhoneNo : Joi.string().trim().min(10).max(13).required(),
-    coDriverEmail : Joi.string().email().trim().min(16).max(50).required(),
+    coDriverEmail : Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).trim().min(16).max(50).required(),
     coCaptainName : Joi.string().trim().min(3).max(30).required(),
     coCaptainPhoneNo : Joi.string().trim().min(10).max(13).required(),
-    coCcaptainEmail : Joi.string().email().trim().min(16).max(50).required(),
+    coCcaptainEmail : Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).trim().min(16).max(50).required(),
     mentorName : Joi.string().trim().min(3).max(30).required(),
     mentorPhoneNo : Joi.string().trim().min(10).max(13).required(),
-    mentorEmail : Joi.string().email().trim().min(16).max(50).required(),
+    mentorEmail : Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).trim().min(16).max(50).required(),
     address : Joi.string().trim().min(3).max(100).required(),
     college : Joi.string().trim().min(3).max(100).required(),
     additionalMembers : Joi.object({
         name : Joi.string().trim().min(3).max(30).required(),
         phoneNo : Joi.string().trim().min(10).max(13).required(),
-        email : Joi.string().email().trim().min(16).max(50).required(),
+        email : Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }).trim().min(16).max(50).required(),
     }),
     payment : Joi.object({
         txnId : Joi.string().trim().min(3).max(100).required(),
