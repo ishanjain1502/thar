da0b4f1041bd63ef3c8d15bda0b2a679e459c43b
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-01-18 11:12:03 +0530

    Rulebook + Thar button fix

--- pages/gokart.js
+++ pages/gokart.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 export default function gokart() {
@@ -6,15 +7,17 @@ export default function gokart() {
       <div className="absolute md:fixed w-max  md:bg-black mx-auto flex items-center rounded-full md:bg-opacity-20 md:ring-1 md:ring-[#BEBEBEa0] md:backdrop-blur-xl top-8 md:left-1/2 md:-translate-x-1/2">
         <ul className="flex flex-col items-start md:flex-row gap-4 md:items-center justify-center px-8 py-2 font-semibold text-xl z-50 ">
           <li>
-            <a href="">
+            <Link href="/">
               <span className="text-red-400">Thar</span>
+            </Link>
+          </li>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="https://drive.google.com/file/d/1VxzDOubqE4xQovtSFa8gsj3cscCa7RbU/view?usp=sharing">
+              Rulebook
             </a>
-          </li>
-          <li>
-            <a href="">Home</a>
-          </li>
-          <li>
-            <a href="">Rulebook</a>
           </li>
           <li>
             <a href="">Glimpse</a>
