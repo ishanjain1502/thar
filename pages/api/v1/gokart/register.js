7e0b5d9158df42238a6c4ca87afe59484bd1ac61
Gulshan Aggarwal <gulshanaggarwal333@gmail.com>
2023-01-27 20:40:02 +0530

    fix: gokart file name case error fixd + img warning disabled

--- pages/api/v1/gokart/register.js
+++ pages/api/v1/gokart/register.js
@@ -1,6 +1,6 @@
 import connectDB from '../../../../lib/mongodb'
 import withValidation from '../../../../middleware/withValidation'
-import validationSchema from '../../../../validators/v1/events/goKart'
+import validationSchema from '../../../../validators/v1/events/gokart'
 import goKart from '../../../../schema/events/gokart';
 import { handleError } from '../../../../utility/handleError';
 
@@ -12,9 +12,12 @@ const handler = async(req,res) => {
         const newTeam = await goKart.create({captainEmail:req.session.user.email, ...req.body})
         res.status(200).json({ error: false, message: 'success',  })
 
-    }catch(err){
-        console.log(err);
-        handleError(res , "error occurred try again!");
+    }catch(error){
+        console.log("error in gokart-register.js page----",error);
+        if (error.code === 11000) {
+            return res.status(403).json({ error: true, message: 'You have already registered!' })
+        }
+        handleError(res,"error occurred try again!");
     }
 }
 
