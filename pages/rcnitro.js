c89b03fd50edcbaecaa62fda814210001daca178
rahul meena <rahulbadgoti000@gmail.com>
2023-02-04 23:40:10 +0530

    rcnitro registration form done

--- pages/rcnitro.js
+++ pages/rcnitro.js
@@ -1,12 +1,17 @@
 import Head from "next/head";
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../components/globals/Footer";
 import { NavBar } from "../components/globals/NavBar";
 import Faq from "../components/rcnitro/Faq";
 import Hero from "../components/rcnitro/Hero";
 import Team from "../components/rcnitro/Team";
+import RcNitroForm from "../components/rcnitro/RcNitroForm";
 
-export default function gokart() {
+
+export default function rcnitro() {
+
+  const [btnClicked,setBtnClicked] = useState(false);
+
   return (
     <>
       <div className="rcnitro">
@@ -14,8 +19,9 @@ export default function gokart() {
           <title>THAR RC NITRO Championship</title>
         </Head>
         <NavBar />
-        <Hero />
+        <Hero setBtnClicked={setBtnClicked} />
         <Team />
+        {btnClicked && <RcNitroForm setBtnClicked={setBtnClicked}/>}
         <Faq />
         <Footer />
       </div>
