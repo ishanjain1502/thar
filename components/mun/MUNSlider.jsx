b058577570c2538b5ec50fc6986e9e82ba526a81
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-02-08 00:33:32 +0530

    agenda popup

--- components/mun/MUNSlider.jsx
+++ components/mun/MUNSlider.jsx
@@ -1,11 +1,7 @@
 import Image from "next/image";
-import React, { useState } from "react";
+import React from "react";
 
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// import required modules
-import { Pagination, Navigation, Autoplay } from "swiper";
+import Modal from "./Modal";
 
 export default function MUNSlider() {
   let arr = [1, 1, 1, 1, 1];
@@ -13,6 +9,8 @@ export default function MUNSlider() {
     {
       name: "UNSC",
       desc: "The United Nations Security Council is one of the six principal organs of the UN. Its primary objective is the maintenance of international peace and security. The UNSC is the only committee with legally binding authority. Members in the SC can impose sanctions and authorize the use of military power through directives or in the resolution itself. The SC also has the responsibility of accepting new member states into the UN.",
+      agenda:
+        "Deliberation on the situation in the Middle East with special emphasis on the Palestinian question.",
     },
     {
       name: "MG",
@@ -25,10 +23,14 @@ export default function MUNSlider() {
     {
       name: "DISEC",
       desc: "The Disarmament and International Security Committee  is the First Committee of the United Nations General Assembly. The committee concerns itself with questions of international importance regarding the security and demilitarization throughout all countries and regions, along with ensuring that citizens across the globe remain protected.",
+      agenda:
+        "Deliberating upon the fundamental objectives of cyber laws and cyber-terrorism.",
     },
     {
       name: "AIPPM",
       desc: "All India Political Party Meet is a non-technical but powerful committee. It is a meeting between all the political parties of the nation. Typically called before the session of the Parliament or before the introduction of a bill, this committee aims to arrive at a consensus before the sessions begin.",
+      agenda:
+        "Discussion on status of women in India with special emphasis on criminalisation of marital rape.",
     },
   ];
   return (
@@ -43,44 +45,33 @@ export default function MUNSlider() {
       <div className="w-10/12 md:w-8/12 xl:w-8/12 flex flex-col-reverse lg:flex-row">
         <div
           lang="en"
-          className="w-full text-xl hyphens flex overflow-x-scroll no-scrollbar"
+          className="w-full text-xl hyphens flex-col overflow-x-scroll no-scrollbar"
         >
-          <Swiper
-            slidesPerView={1}
-            spaceBetween={10}
-            loop={true}
-            autoplay={{
-              delay: 2500,
-              disableOnInteraction: false,
-            }}
-            navigation={false}
-            modules={[Autoplay, Pagination, Navigation]}
-            zoom={true}
-            className="mySwiper flex justify-center items-center"
-          >
-            {arr.map((i, index) => (
-              <SwiperSlide
-                key={index}
-                className="mySwiper flex justify-center items-center flex-col md:flex-row overflow-hidden text-black"
-              >
-                <Image
-                  src={`/assets/images/mun/comittees/${index + 1}.png`}
-                  alt=""
-                  quality={100}
-                  width={720}
-                  height={480}
-                  loading="lazy"
-                  className="object-cover rounded-full"
-                />
-                <div>
-                  <h1 className="text-4xl font-bold mb-4 underline">
-                    {committees[index].name}
-                  </h1>
-                  <p>{committees[index].desc}</p>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
+          {arr.map((i, index) => (
+            <div
+              key={index}
+              className="mySwiper flex justify-center items-center flex-col md:flex-row overflow-hidden text-black"
+            >
+              <Image
+                src={`/assets/images/mun/comittees/${index + 1}.png`}
+                alt=""
+                quality={100}
+                width={720}
+                height={480}
+                loading="lazy"
+                className="object-cover rounded-full"
+              />
+              <div>
+                <h1 className="text-4xl font-bold mb-4 underline">
+                  {committees[index].name}
+                </h1>
+                <p>{committees[index].desc}</p>
+                {committees[index].agenda && (
+                  <Modal modalState={false} data={committees[index]} />
+                )}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
