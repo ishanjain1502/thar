1b12807f13ec15cd5d7b8e247585a14d3b464d79
Pushkar Yadav <96358784+pushkarydv@users.noreply.github.com>
2023-02-09 02:22:51 +0530

    events in dahsboard

--- components/participant/ShowParticipant.js
+++ components/participant/ShowParticipant.js
@@ -1,9 +1,21 @@
 import { signOut } from "next-auth/react";
 import { useRouter } from "next/router";
 import React from "react";
+import { events_data } from "../../data/events";
 
 export default function ShowParticipant({ userData }) {
   const router = useRouter();
+
+  /* Array of filtered events of user on basis of event id recieved from db */
+  var user_events = [];
+  userData.events.forEach((event) => {
+    let event_data = events_data.filter((obj) => obj.id == event);
+    if (!user_events.includes(event_data[0])) {
+      user_events.push(event_data[0]);
+    }
+  });
+  console.log(user_events);
+
   return (
     <>
       <div className="text-center">
@@ -51,9 +63,28 @@ export default function ShowParticipant({ userData }) {
           </div>
         </div>
       </div>
-      <div className="text-slate-700 font-semibold px-4 py-2 md:text-center">
+      <div className="text-slate-700 font-semibold px-4 py-1 md:text-center">
         Credits are used to participate in events.
       </div>
+
+      {/* Show events data if user has registered in any event */}
+      {userData.events.length > 0 && (
+        <div className="bg-emerald-200 m-4 p-4 rounded-md">
+          <p className="text-lg text-emerald-900 font-semibold">Your events:</p>
+          <ul className="mt-2 list-none">
+            {/* {events_data[0].name} */}
+            {user_events.map((event, index) => (
+              <li key={event.id} className="text-lg">
+                {index + 1}. {event.name}{" "}
+                <span className="bg-sky-500 text-white px-2 text-sm rounded-full">
+                  {event.event_type}
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
       <button
         className="transition-all w-fit mx-auto bg-black font-semibold text-white rounded-lg py-2 px-4  justify-center items-center mt-4 active:scale-95 flex flex-row gap-2"
         onClick={() => signOut({ callbackUrl: "/" })}
